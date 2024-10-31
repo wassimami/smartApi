259d46f82f4da428b996c3e010f8c20451c6de88
wassimami <147424558+wassimami@users.noreply.github.com>
2024-10-31 15:01:11 +0100

    added api to the backend

--- server.js
+++ server.js
@@ -3,7 +3,50 @@ import bcrypt from 'bcrypt-nodejs';
 import cors from 'cors';
 import knex from 'knex';
 import fetch from 'node-fetch';
-import Clarifai from 'clarifai'
+
+
+
+const setupClarifai = (imageUrl) => {
+
+// Your PAT (Personal Access Token) can be found in the Account's Security section
+const PAT = 'fc7d0cdd35754a598b040ddd372a8ad3';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'i9ufkd1cgej1';
+const APP_ID = 'test';
+// Change these to whatever model and image URL you want to use
+
+//const MODEL_ID = 'face-detection';
+const IMAGE_URL = imageUrl;
+
+
+const raw = JSON.stringify({
+  "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+  },
+  "inputs": [
+      {
+          "data": {
+              "image": {
+                  "url": IMAGE_URL
+                  // "base64": IMAGE_BYTES_STRING
+              }
+          }
+      }
+  ]
+});
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+return requestOptions;
+
+}
 
 
 const pgdb =knex({
@@ -110,6 +153,14 @@ app.put('/image', (req,res)=>{
 	pgdb('users').where('id', '=', id).increment('entries', 1)
 	.returning('entries').then(entries=>{res.json(entries[0].entries)})
 	.catch(err => res.status(400).json('unable to get count'))
+})
+
+app.post('/imageu' ,(req,res)=>{
+	fetch("https://api.clarifai.com/v2/models/face-detection/outputs", setupClarifai(req.body.input))
+    .then(data => {
+    	res.json(data);
+  })
+    .catch(err => res.status(400).json('unable for api'))
 })
 
 
